bd7747348aa235dce65120c6821ffea62da93b73
Varoni <varonidiogo@gmail.com>
2022-11-10 23:45:27 -0300

    Criação do componente de 'vendedores' e 'departamento'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VendedoresComponent } from './vendedores/vendedores.component';
+import { DepartamentosComponent } from './departamentos/departamentos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    VendedoresComponent,
+      DepartamentosComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
