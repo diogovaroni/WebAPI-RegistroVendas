5eb30246f6dbb0b9ad128ddc8dcf7b7e613f472d
Varoni <varonidiogo@gmail.com>
2022-11-14 16:29:43 -0300

    Implementação dropdow barra de navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { VendasComponent } from './vendas/vendas.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavComponent } from './nav/nav.component';
 import { TitleComponent } from './title/title.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 @NgModule({
   declarations: [					
@@ -22,7 +25,9 @@ import { TitleComponent } from './title/title.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
